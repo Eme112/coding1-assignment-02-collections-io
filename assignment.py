9abb2746e3c4c477db7ba448f4dff9290dd6e449
Eme112 <eme_pedraza@hotmail.com>
2026-09-23 17:10:20 +0200

    Added some comments to assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # This creates a string variable with the value "tea"
+sales = {"tea": 3, "coffee": 5} # I think this might be a dictionary that maps string values to numeric values
+sold = sales[drink] # This is assigning the numeric value 3 to the variable sold
+sold = sold + 1 # This is increasing the value of variable sold from 3 to 4
+print(drink) # This should print "tea"
+print(sold) # This should print "4"
